4aeaadf638083ea4a5b204fd637645bd62fc0ea5
Franck Duchè <frankduche@gmail.com>
2019-07-27 15:39:03 +0200

    Add article controller and 2 mock up routes

---
+++ app/controllers/article_controller.rb
@@ -0,0 +1,10 @@
+class ArticleController < ApplicationController
+  def new
+ 
+  end
+  
+  def new2
+  
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'article/new2'
+  resource :article, only: [:new]
+  
+  #get 'article/new', to: 'article#new'
+  
 end
